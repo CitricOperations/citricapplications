34b14c5f058f4160ec00f14a6c6d059cc44f41c5
Rodney Thomas <116589621+rodrthomas@users.noreply.github.com>
2022-10-27 14:39:51 -0500

    Create Services.java

---
+++ Services.java
@@ -0,0 +1,37 @@
+import java.awt.Color;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.font.*;
+import java.awt.*;
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.util.concurrent.TimeUnit;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Services {
+
+
+  /*
+  *
+  * Defines sleep action, which pauses the program temporarily, can be called with wait(time in ms);
+  *
+  */
+  public static void wait(int ms) {
+    try {
+      Thread.sleep(ms);
+    }
+    catch(InterruptedException ex) {
+      Thread.currentThread().interrupt();
+    }
+  }
+  
+  static public void main(String[] args) {
+
+    
+  }
+  
+}
