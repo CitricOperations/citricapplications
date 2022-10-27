9eab84879e6539c59cac98cea6913bd5077d74a2
Rodney Thomas <116589621+rodrthomas@users.noreply.github.com>
2022-10-27 11:06:03 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,53 +7,86 @@ import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     Home();
+  }
+
+  public static void wait(int ms) {
+    try {
+      Thread.sleep(ms);
+    }
+    catch(InterruptedException ex) {
+      Thread.currentThread().interrupt();
+    }
   }
   
   static void Home() {
 
-    GraphicsEnvironment graphics =
-      GraphicsEnvironment.getLocalGraphicsEnvironment();
+
+    Font font1 = new Font("DialogInput", Font.BOLD, 80);
+    GraphicsEnvironment graphics = GraphicsEnvironment.getLocalGraphicsEnvironment();
     GraphicsDevice device = graphics.getDefaultScreenDevice();
 
-    
     JLabel homelabel = new JLabel("Home");
     homelabel.setHorizontalAlignment(SwingConstants.CENTER);
     homelabel.setVerticalAlignment(SwingConstants.CENTER);
     homelabel.setForeground(Color.WHITE);
+    homelabel.setFont(font1);
+    homelabel.setIcon(new ImageIcon("OrangeLogo.png"));
+    homelabel.setText("CITRUS");
 
-    String newline = System.getProperty("line.seperator");
-    
     javax.swing.JFrame frame = new JFrame();
     frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
     frame.setUndecorated(true);
     frame.setResizable(false);
     frame.setVisible(true);
-    homelabel.setText("CITRIC");
     frame.add(homelabel);
-    Font font1 = new Font("Arial", Font.BOLD, 50);
-    homelabel.setFont(font1);
     frame.getContentPane().setBackground(new java.awt.Color(44, 47, 51));
     frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-
     /*
+     * 
+     * The code below will be used to CLOSE THE MAIN WINDOW when any application is
+     * opened.
+     * 
+     */
 
-    The code below will be used to CLOSE THE MAIN WINDOW when any application is opened.
 
-    */
-    
-    // frame.setVisible(false);
-    // frame.dispose();
+    wait(3000);
+    frame.setVisible(false);
+    frame.dispose();
+    CopyrightScreen();
+  }
+
+  static void CopyrightScreen() {
+    GraphicsEnvironment graphics = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    GraphicsDevice device = graphics.getDefaultScreenDevice();
+
+    Font font3 = new Font("DialogInput", Font.BOLD, 15);
+    JLabel copyrightlabel = new JLabel("Copyright");
+    copyrightlabel.setHorizontalAlignment(SwingConstants.RIGHT);
+    copyrightlabel.setVerticalAlignment(SwingConstants.CENTER);
+    copyrightlabel.setForeground(Color.WHITE);
+    copyrightlabel.setText("(C)INFRARED STUDIOS");
+    copyrightlabel.setFont(font3);
+
+    javax.swing.JFrame copyrightFrame = new JFrame();
+    copyrightFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+    copyrightFrame.setUndecorated(true);
+    copyrightFrame.setResizable(false);
+    copyrightFrame.setVisible(true);
+    copyrightFrame.getContentPane().setBackground(new java.awt.Color(44, 47, 51));
+    copyrightFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+    copyrightFrame.add(copyrightlabel);
   }
 
   static void CitricServices() {
-    GraphicsEnvironment graphics =
-      GraphicsEnvironment.getLocalGraphicsEnvironment();
+    GraphicsEnvironment graphics = GraphicsEnvironment.getLocalGraphicsEnvironment();
     GraphicsDevice device = graphics.getDefaultScreenDevice();
 
     Font font2 = new Font("Arial", Font.BOLD, 40);
